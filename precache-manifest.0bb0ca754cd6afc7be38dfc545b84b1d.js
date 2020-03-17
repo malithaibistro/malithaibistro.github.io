2b40cb4960b23fa06b0b7635d51e69268fc6baa6
Keen <kwilson@finsignum.com>
2020-03-17 13:13:07 -0500

    Set display none to mobile component when the screen is desktop

---
+++ precache-manifest.0bb0ca754cd6afc7be38dfc545b84b1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5c02b512b1173c82144246bd640e6a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f2acdc09a394626e7e3",
+    "url": "/static/css/2.444de3e9.chunk.css"
+  },
+  {
+    "revision": "54ff0437051eb442fb47",
+    "url": "/static/css/main.b881b995.chunk.css"
+  },
+  {
+    "revision": "9f2acdc09a394626e7e3",
+    "url": "/static/js/2.b16f9413.chunk.js"
+  },
+  {
+    "revision": "54ff0437051eb442fb47",
+    "url": "/static/js/main.b75eb69f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
