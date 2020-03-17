3c936c6d58f9435c707cfd7242b07d805c895122
Keen <kwilson@finsignum.com>
2020-03-17 13:07:47 -0500

    Update test to Mali Thai Bistro in announcement

---
+++ precache-manifest.2e368565018b7681f357dc678b00ab5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32e415f04c080c12074e94d5028b442a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f2acdc09a394626e7e3",
+    "url": "/static/css/2.444de3e9.chunk.css"
+  },
+  {
+    "revision": "7ec8cc9fe195f42dfc99",
+    "url": "/static/css/main.c0c92a66.chunk.css"
+  },
+  {
+    "revision": "9f2acdc09a394626e7e3",
+    "url": "/static/js/2.b16f9413.chunk.js"
+  },
+  {
+    "revision": "7ec8cc9fe195f42dfc99",
+    "url": "/static/js/main.b75eb69f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
