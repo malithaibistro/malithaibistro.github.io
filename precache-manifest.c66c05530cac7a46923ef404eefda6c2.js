7eeec1bafc59bde20cc4d9dc245ad429688456b5
Keen <keencwilson@gmail.com>
2025-07-29 19:38:12 -0700

    Deploy updated Mali Thai Bistro website with PDF menu

---
+++ precache-manifest.c66c05530cac7a46923ef404eefda6c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf207b8ed0b85131132e692fa056beb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9d825e4d95056e3c9d2",
+    "url": "/static/css/2.4eb4fdfa.chunk.css"
+  },
+  {
+    "revision": "462d4b7bf4f3442c4243",
+    "url": "/static/css/main.218fc0db.chunk.css"
+  },
+  {
+    "revision": "e9d825e4d95056e3c9d2",
+    "url": "/static/js/2.a692a483.chunk.js"
+  },
+  {
+    "revision": "462d4b7bf4f3442c4243",
+    "url": "/static/js/main.ebfa0db2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
